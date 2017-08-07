1b000e836cb7e893e5cf17f30b3c6fbbf1ce805b
Yifan Jia <982151756@qq.com>
2017-08-07 09:59:58 +0800

    Update 1026.cpp

--- 1026.cpp
+++ 1026.cpp
@@ -1,217 +1,128 @@
-#include<iostream>
-#include<queue>
-#include<vector>
-#include<algorithm>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <cmath>
 using namespace std;
-typedef struct people
-{
-	int arrivetime,playtime,servetime,vipflag;
-	float waittime;
-}A;
-int cmp(A a,A b)
-{
-	return a.arrivetime<b.arrivetime;
+struct person {
+    int arrive, start, time;
+    bool vip;
+}tempperson;
+struct tablenode {
+    int end = 8 * 3600;
+    int num;
+    bool vip;
+};
+bool cmp1(person a, person b) {
+    return a.arrive < b.arrive;
 }
-int viptable[105],num[4];
-int main()
-{
-	int n,i,j,k,m;
-	cin>>n;
-	vector<A> a(n);
-	for(i=0;i<n;i++)
-	{
-		int hh,mm,ss,time;
-		scanf("%d:%d:%d %d %d",&hh,&mm,&ss,&a[i].playtime,&a[i].vipflag);
-		if(a[i].playtime>=120) a[i].playtime=120;
-		a[i].playtime*=60;
-		time=hh*3600+mm*60+ss;
-		a[i].arrivetime=time;
-	}
-	cin>>k>>m;
-	for(i=0;i<m;i++)
-	{
-		int temp;
-		cin>>temp;
-		viptable[temp]=1;
-	}
-	queue<A> ordinary,vip,pp;
-	vector<int> table(k+1,28800);
-	sort(a.begin(),a.end(),cmp);
-	for(i=0;i<a.size();i++)
-		pp.push(a[i]);
-	while(true)
-	{
-		int index,mintable=999999999;
-		for(i=1;i<=k;i++)
-		{
-			if(table[i]<mintable)
-			{
-				index=i;
-				mintable=table[i];
-			}
-		}
-		//cout<<index<<endl;
-		if(table[index]>=75600) break;
-		while(pp.empty()==false&&pp.front().arrivetime<table[index])
-		{
-			if(pp.front().vipflag==0)
-			{
-				ordinary.push(pp.front());
-				pp.pop();
-			}
-			else if(pp.front().vipflag==1)
-			{
-				vip.push(pp.front());
-				pp.pop();
-			}
-		}
-		if(ordinary.empty()==true&&vip.empty()==true)
-		{
-			table[index]=pp.front().arrivetime+pp.front().playtime;
-			pp.front().servetime=pp.front().arrivetime;
-			pp.front().waittime=0;
-			int hh1,mm1,ss1;
-			hh1=pp.front().arrivetime/3600;
-			pp.front().arrivetime%=3600;
-			mm1=pp.front().arrivetime/60;
-			ss1=pp.front().arrivetime%60;
-			printf("%02d:%02d:%02d %02d:%02d:%02d %.0f\n",hh1,mm1,ss1,hh1,mm1,ss1,pp.front().waittime);
-			pp.pop();
-			num[index]++;	
-		}
-		else if(viptable[index]==1)
-		{
-			if(vip.empty()==false)
-			{
-				int temp=table[index];
-				vip.front().waittime=(table[index]-vip.front().arrivetime)/60.0+0.1;
-				//cout<<vip.front().arrivetime<<" "<<table[index]<<endl;
-				//cout<<endl;
-				int h1,m1,s1,h2,m2,s2;
-				h1=vip.front().arrivetime/3600;
-				vip.front().arrivetime%=3600;
-				m1=vip.front().arrivetime/60;
-				s1=vip.front().arrivetime%60;
-				h2=temp/3600;
-				temp%=3600;
-				m2=temp/60;
-				s2=temp%60;
-				printf("%02d:%02d:%02d %02d:%02d:%02d %.0f\n",h1,m1,s1,h2,m2,s2,vip.front().waittime);
-				table[index]+=vip.front().playtime;
-				vip.pop();
-				num[index]++;
-			}
-			else
-			{
-				int temp=table[index];
-				ordinary.front().waittime=(table[index]-ordinary.front().arrivetime)/60.0+0.1;
-				//cout<<vip.front().arrivetime<<" "<<table[index]<<endl;
-				//cout<<endl;
-				int h1,m1,s1,h2,m2,s2;
-				h1=ordinary.front().arrivetime/3600;
-				ordinary.front().arrivetime%=3600;
-				m1=ordinary.front().arrivetime/60;
-				s1=ordinary.front().arrivetime%60;
-				h2=temp/3600;
-				temp%=3600;
-				m2=temp/60;
-				s2=temp%60;
-				printf("%02d:%02d:%02d %02d:%02d:%02d %.0f\n",h1,m1,s1,h2,m2,s2,ordinary.front().waittime);
-				table[index]+=ordinary.front().playtime;
-				ordinary.pop();
-				num[index]++;
-			}
-		}
-		else if(viptable[index]==0)
-		{
-			if(vip.empty()==true)
-			{
-				int temp=table[index];
-				ordinary.front().waittime=(table[index]-ordinary.front().arrivetime)/60.0+0.1;
-				//cout<<vip.front().arrivetime<<" "<<table[index]<<endl;
-				//cout<<endl;
-				int h1,m1,s1,h2,m2,s2;
-				h1=ordinary.front().arrivetime/3600;
-				ordinary.front().arrivetime%=3600;
-				m1=ordinary.front().arrivetime/60;
-				s1=ordinary.front().arrivetime%60;
-				h2=temp/3600;
-				temp%=3600;
-				m2=temp/60;
-				s2=temp%60;
-				printf("%02d:%02d:%02d %02d:%02d:%02d %.0f\n",h1,m1,s1,h2,m2,s2,ordinary.front().waittime);
-				table[index]+=ordinary.front().playtime;
-				ordinary.pop();
-				num[index]++;
-			}
-			else if(ordinary.empty()==true)
-			{
-				int temp=table[index];
-				vip.front().waittime=(table[index]-vip.front().arrivetime)/60.0+0.1;
-				//cout<<vip.front().arrivetime<<" "<<table[index]<<endl;
-				//cout<<endl;
-				int h1,m1,s1,h2,m2,s2;
-				h1=vip.front().arrivetime/3600;
-				vip.front().arrivetime%=3600;
-				m1=vip.front().arrivetime/60;
-				s1=vip.front().arrivetime%60;
-				h2=temp/3600;
-				temp%=3600;
-				m2=temp/60;
-				s2=temp%60;
-				printf("%02d:%02d:%02d %02d:%02d:%02d %.0f\n",h1,m1,s1,h2,m2,s2,vip.front().waittime);
-				table[index]+=vip.front().playtime;
-				vip.pop();
-				num[index]++;
-			}
-			else if(ordinary.front().arrivetime<vip.front().arrivetime)
-			{
-				int temp=table[index];
-				ordinary.front().waittime=(table[index]-ordinary.front().arrivetime)/60.0+0.1;
-				//cout<<vip.front().arrivetime<<" "<<table[index]<<endl;
-				//cout<<endl;
-				int h1,m1,s1,h2,m2,s2;
-				h1=ordinary.front().arrivetime/3600;
-				ordinary.front().arrivetime%=3600;
-				m1=ordinary.front().arrivetime/60;
-				s1=ordinary.front().arrivetime%60;
-				h2=temp/3600;
-				temp%=3600;
-				m2=temp/60;
-				s2=temp%60;
-				printf("%02d:%02d:%02d %02d:%02d:%02d %.0f\n",h1,m1,s1,h2,m2,s2,ordinary.front().waittime);
-				table[index]+=ordinary.front().playtime;
-				ordinary.pop();
-				num[index]++;
-			}
-			else
-			{
-				int temp=table[index];
-				vip.front().waittime=(table[index]-vip.front().arrivetime)/60.0+0.1;
-				//cout<<vip.front().arrivetime<<" "<<table[index]<<endl;
-				//cout<<endl;
-				int h1,m1,s1,h2,m2,s2;
-				h1=vip.front().arrivetime/3600;
-				vip.front().arrivetime%=3600;
-				m1=vip.front().arrivetime/60;
-				s1=vip.front().arrivetime%60;
-				h2=temp/3600;
-				temp%=3600;
-				m2=temp/60;
-				s2=temp%60;
-				printf("%02d:%02d:%02d %02d:%02d:%02d %.0f\n",h1,m1,s1,h2,m2,s2,vip.front().waittime);
-				table[index]+=vip.front().playtime;
-				vip.pop();
-				num[index]++;
-			}
-		}
-		if(pp.empty()==true&&ordinary.empty()==true&&vip.empty()==true)
-			break;	
-	}
-	for(i=1;i<=k;i++)
-	{
-		if(i==1) cout<<num[i];
-		else cout<<" "<<num[i];
-	}
-	return 0;
+bool cmp2(person a, person b) {
+    return a.start < b.start;
+}
+vector<person> player;
+vector<tablenode> table;
+void alloctable(int personid, int tableid) {
+    if(player[personid].arrive <= table[tableid].end)
+        player[personid].start = table[tableid].end;
+    else
+        player[personid].start = player[personid].arrive;
+    table[tableid].end = player[personid].start + player[personid].time;
+    table[tableid].num++;
+}
+int findnextvip(int vipid) {
+    vipid++;
+    while(vipid < player.size() && player[vipid].vip == false) {
+        vipid++;
+    }
+    return vipid;
+}
+int main() {
+    int n, k, m, viptable;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++) {
+        int h, m, s, temptime, flag;
+        scanf("%d:%d:%d %d %d", &h, &m, &s, &temptime, &flag);
+        tempperson.arrive = h * 3600 + m * 60 + s;
+        tempperson.start = 21 * 3600;
+        if(tempperson.arrive >= 21 * 3600)
+            continue;
+        tempperson.time = temptime <= 120 ? temptime * 60 : 7200;
+        tempperson.vip = ((flag == 1) ? true : false);
+        player.push_back(tempperson);
+    }
+    scanf("%d%d", &k, &m);
+    table.resize(k + 1);
+    for(int i = 0; i < m; i++) {
+        scanf("%d", &viptable);
+        table[viptable].vip = true;
+    }
+    sort(player.begin(), player.end(), cmp1);
+    int i = 0, vipid = -1;
+    vipid = findnextvip(vipid);
+    while(i < player.size()) {
+        int index = -1, minendtime = 999999999;
+        for(int j = 1; j <= k; j++) {
+            if(table[j].end < minendtime) {
+                minendtime = table[j].end;
+                index = j;
+            }
+        }
+        if(table[index].end >= 21 * 3600)
+            break;
+        if(player[i].vip == true && i < vipid) {
+            i++;
+            continue;
+        }
+        if(table[index].vip == true) {
+            if(player[i].vip == true) {
+                alloctable(i, index);
+                if(vipid == i)
+                    vipid = findnextvip(vipid);
+                i++;
+            } else {
+                if(vipid < player.size() && player[vipid].arrive <= table[index].end) {
+                    alloctable(vipid, index);
+                    vipid = findnextvip(vipid);
+                } else {
+                    alloctable(i, index);
+                    i++;
+                }
+            }
+        } else {
+            if(player[i].vip == false) {
+                alloctable(i, index);
+                i++;
+            } else {
+                int vipindex = -1, minvipendtime = 999999999;
+                for(int j = 1; j <= k; j++) {
+                    if(table[j].vip == true && table[j].end < minvipendtime) {
+                        minvipendtime = table[j].end;
+                        vipindex = j;
+                    }
+                }
+                if(vipindex != -1 && player[i].arrive >= table[vipindex].end) {
+                    alloctable(i, vipindex);
+                    if(vipid == i)
+                        vipid = findnextvip(vipid);
+                    i++;
+                } else {
+                    alloctable(i, index);
+                    if(vipid == i)
+                        vipid = findnextvip(vipid);
+                    i++;
+                }
+            }
+        }
+    }
+    sort(player.begin(), player.end(), cmp2);
+    for(i = 0; i < player.size() && player[i].start < 21 * 3600; i++) {
+        printf("%02d:%02d:%02d ", player[i].arrive / 3600, player[i].arrive % 3600 / 60, player[i].arrive % 60);
+        printf("%02d:%02d:%02d ", player[i].start / 3600, player[i].start % 3600 / 60, player[i].start % 60);
+        printf("%.0f\n", round((player[i].start - player[i].arrive) / 60.0));
+    }
+    for(int i = 1; i <= k; i++) {
+        if(i != 1)
+            printf(" ");
+        printf("%d", table[i].num);
+    }
+    return 0;
 }
