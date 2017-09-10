2375ae3abbf992f666cb57c20e2a443fbbb7d461
Yifan Jia <982151756@qq.com>
2017-09-10 14:02:11 +0800

    Update 1057.cpp

--- 1057.cpp
+++ 1057.cpp
@@ -1,61 +1,62 @@
-#include <stack>
-#include <vector>
-#include <string>
-#include <iostream>
-#include <algorithm>
+#include<iostream>
+#include<stack>
 using namespace std;
-const int maxn = 100000 + 10;
-const int sqrN = 316;
+#define lowbit(i) ((i)&(-i))
+const int maxn=100010;
 stack<int> s;
-int block[sqrN];
-int table[maxn];
-void Pop(){
-int elem;
-if(s.empty())
-cout << "Invalid" << endl;
-else{
-elem = s.top();
-table[elem] --;
-block[elem/sqrN] --;
-s.pop();
-cout << elem << endl;
+int c[maxn];
+void update(int x,int v)
+{
+	for(int i=x;i<maxn;i+=lowbit(i))
+		c[i]+=v;
 }
+int getsum(int x)
+{
+	int sum=0;
+	for(int i=x;i>0;i-=lowbit(i))
+		sum+=c[i];
+	return sum;
 }
-void Push(){
-int elem;
-cin >> elem;
-table[elem] ++;
-block[elem/sqrN] ++;
-s.push(elem);
+void peekmedian()
+{
+	int l=1,r=maxn,mid,k=(s.size()+1)/2;
+	while(l<r)
+	{
+		mid=(l+r)/2;
+		if(getsum(mid)>=k) r=mid;
+		else l=mid+1;
+	}
+	printf("%d\n",l);
 }
-void PeekMedian(){
-if(s.empty())
-cout << "Invalid" << endl;
-else{
-int sum_ = 0;
-int idx = 0;
-int len = s.size();
-int mid = (len % 2 == 0) ? (len / 2) : ((len + 1) / 2);
-while(sum_ + block[idx] < mid)
-sum_ += block[idx++];
-int num = idx * sqrN;
-while(sum_ + table[num] < mid)
-sum_ += table[num++];
-printf("%d\n", num);
-}
-}
-int main(){
-int n;
-cin >> n;
-char op[15];
-while(n--){
-scanf("%s", op);
-if(op[1] == 'o')
-Pop();
-else if(op[1] == 'u')
-Push();
-else if(op[1] == 'e')
-PeekMedian();
-}
-return 0;
+int main()
+{
+	int n,x;
+	char str[15];
+	scanf("%d",&n);
+	for(int i=0;i<n;i++)
+	{
+		scanf("%s",str);
+		if(str[1]=='u')
+		{
+			scanf("%d",&x);
+			s.push(x);
+			update(x,1);
+		}
+		else if(str[1]=='o')
+		{
+			if(s.empty()) printf("Invalid\n");
+			else
+			{
+				printf("%d\n",s.top());
+				update(s.top(),-1);
+				s.pop();
+			}
+		}
+		else
+		{
+			if(s.empty()) printf("Invalid\n");
+			else peekmedian();
+		}
+	}
+	return 0;
 }
